a369b2c9a3c67e7394ace1301e5aee77919a4374
Mathias Greve Johannessen <mathigj@ulrik.uio.no>
2024-12-30 23:01:38 +0100

    add templates and password reset functionalty

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -7,7 +7,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add_entry, name='add_entry'),
-    #path('accounts/', include('django.contrib.auth.urls')),  # Login/logout views
+    path('accounts/', include('django.contrib.auth.urls')),  # Login/logout views
     path('login/', auth_views.LoginView.as_view(next_page='home'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('signup/', views.signup, name='signup'),  # Add this line for the signup page
