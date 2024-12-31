858b80d6933840a7d4aaf77825d67dc1f90b9499
Mathias Greve Johannessen <mathigj@ulrik.uio.no>
2025-01-01 00:20:13 +0100

    comment out obsolete login/logout urls

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add_entry, name='add_entry'),
     path('accounts/', include('django.contrib.auth.urls')),  # Login/logout views
-    path('login/', auth_views.LoginView.as_view(next_page='home'), name='login'),    # stock view - change ?
-    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'), # stock view - change?
+    #path('login/', auth_views.LoginView.as_view(next_page='home'), name='login'),    # stock view - change ?
+    #path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'), # stock view - change?
     path('signup/', views.signup, name='signup'),  # Add this line for the signup page
     path('upvote/<int:entry_id>/', views.upvote_entry, name='upvote_entry'),
     path('downvote/<int:entry_id>/', views.downvote_entry, name='downvote_entry'),
