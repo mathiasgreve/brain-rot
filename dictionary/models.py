124947ff85273aeec93a380215f2055bc42a4c79
Mathias Greve Johannessen <mathigj@ulrik.uio.no>
2024-12-31 01:33:02 +0100

    expand model with example

--- dictionary/models.py
+++ dictionary/models.py
@@ -15,6 +15,13 @@ class Entry(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Example(models.Model):
+    entry = models.ForeignKey(Entry, on_delete=models.CASCADE, related_name="examples")
+    text = models.TextField()
+
+    def __str__(self):
+        return f"Example for {self.entry.title}: {self.text[:30]}..."
 
 class Vote(models.Model):
     UPVOTE = 1
