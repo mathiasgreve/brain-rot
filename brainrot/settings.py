7e67af9c7966856c4207748514fd56efcb53b1c2
Mathias Greve Johannessen <mathigj@ulrik.uio.no>
2025-01-01 13:01:18 +0100

    set login/out redirection

--- brainrot/settings.py
+++ brainrot/settings.py
@@ -138,3 +138,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
+
+LOGIN_REDIRECT_URL = '/'  #redirects to home page
+LOGOUT_REDIRECT_URL = '/' #redirects to home page
